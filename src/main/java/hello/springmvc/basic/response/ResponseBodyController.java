44b39a04e77f3306343387fe3a678b8ba62101a1
woodiePlanit <woodie@planit.ai>
2022-04-12 16:54:49 +0900

    initial

---
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,49 @@
+package hello.springmvc.basic.response;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @GetMapping("/response-body-string-v2")
+    public String responseBodyV2(@RequestParam String tt) throws IOException {
+
+        log.info("{}",tt);
+        return tt;
+    }
+
+    @ResponseBody
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3() throws IOException {
+        return "ok";
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/response-body-string-v4")
+    public String responseBodyV4Json() throws IOException {
+
+        return "testJson";
+    }
+
+
+
+
+}
